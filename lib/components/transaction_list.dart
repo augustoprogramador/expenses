e85aca2ba4ac3ecae1488e117e4031da020b814f
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2023-10-07 22:33:06 -0300

    Refatorando a Listagem e alterando o Scroll

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -14,56 +14,59 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     initializeDateFormatting('pt_BR');
-    return Column(
-      children: transactions
-          .map(
-            (tr) => Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.purple,
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 2,
-                        color: Colors.purple,
-                      ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title,
                       style: const TextStyle(
-                        color: Colors.purple,
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                        fontSize: 16,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
+                    Text(
+                      DateFormat(' d MMM y').format(tr.date),
+                      style: const TextStyle(
+                        color: Colors.grey,
                       ),
-                      Text(
-                        DateFormat(' d MMM y').format(tr.date),
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          )
-          .toList(),
+          );
+        },
+      ),
     );
   }
 }
